7dcf7827000f742112db38f6d78f0b506d786537
Sarah <sarah.simon@ynov.com>
2017-05-09 15:12:16 +0200

    modif

--- js.js
+++ js.js
@@ -1,46 +1,36 @@
-function machineASous(){
+function jeu(){
 	/* Récupération du crédit et du score. Il est fait à chaque fois
 	car cela change en fonction des images qui ont été affichées au
 	tirage précédent */
-	var creditJoueur = document.getElementById("credit").value;
-	var highscore = document.getElementById("highscore").value;
+	var credit = document.getElementById("credit").value;
+	var score = document.getElementById("score").value;
 	
 	// Génération de 3 nombres aléatoires entre 0 et 3 pour afficher les images
-	var w = Math.floor(Math.random()*4);
-	var x = Math.floor(Math.random()*4);
-	var y = Math.floor(Math.random()*4);
+	var w = Math.floor(Math.random()*5);
+	var x = Math.floor(Math.random()*5);
+	var y = Math.floor(Math.random()*5);
+    var z = Math.floor(Math.random()*5);
 	
 	// Modification de la source des images à l'aide des 3 nombres aléatoires
-	document.getElementById("img_sous_1").src = 'img_' + w + '.png';
-	document.getElementById("img_sous_2").src = 'img_' + x + '.png';
-	document.getElementById("img_sous_3").src = 'img_' + y + '.png';
+	document.getElementById("images/carreau.png").src = 'img_' + w + '.png';
+	document.getElementById("images/coeur.png").src = 'img_' + x + '.png';
+	document.getElementById("images/pique.png").src = 'img_' + y + '.png';
+	document.getElementById("images/trefle.png").src = 'img_' + z + '.png';
 	
-	// Le joueur perd 10 crédits à chaque tirage
-	creditJoueur -= 10;
+	// Le joueur perd 1 crédit à chaque tirage
+	creditJoueur -= 1;
 	
-	/* Si les 3 nombres aléatoires sont 0, cela correspond aux têtes de mort,
-	cela veut dire que l'utilisateur perd tous ses crédits, le bouton "GO !"
-	est donc désactivé car il n'a plus de crédit pour jouer */
-	if(w == 0 && x == 0 && y == 0){
-		creditJoueur = 0;
-		document.getElementById("boutonGO").disabled = "disabled";
-	}else 
+	
 	// Si les 3 nombres sont égaux, alors on a 3 mêmes images et l'utilisateur gagne 80
-	if(w == x && x == y){
-		creditJoueur += 80;
-	}else 
-	/* S'il y a 2 nombres égaux, alors l'utilisateur gagne 40. Les caractères "^" permettent
-	un ou exclusif (XOR), cela veut dire soit, la première condition, soit la deuxième, soit
-	la troisième mais pas deux ou les trois en même temps */
-	if(w == x ^ w == y ^ x == y){
-		creditJoueur += 40;
+	if(w == x && x == y && y==z){
+		creditJoueur += 5;
 	}
 	/* Si l'utilisateur a un crédit plus élevé que son highscore, alors le highscore
 	prend la valeur du crédit */
-	if(creditJoueur > highscore){
-		highscore = creditJoueur;
+	if(credit > score){
+		score = credit;
 	}
 	/* On met à jour le crédit et le highscore du joueur */
-	document.getElementById("credit").value = creditJoueur;
-	document.getElementById("highscore").value = highscore;
+	document.getElementById("credit").value = credit;
+	document.getElementById("score").value = score;
 }
