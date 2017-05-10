f2113df973946b8dfcf463509fee0e29ef788aa7
RePloZ <herve.kay@ynov.com>
2017-05-10 10:04:22 +0200

    alpah 0.2

--- app.js
+++ app.js
@@ -1,36 +1,15 @@
-function jeu(){
-	/* Récupération du crédit et du score. Il est fait à chaque fois
-	car cela change en fonction des images qui ont été affichées au
-	tirage précédent */
-	var credit = document.getElementById("credit").value;
-	var score = document.getElementById("score").value;
-	
-	// Génération de 3 nombres aléatoires entre 0 et 3 pour afficher les images
-	var w = Math.floor(Math.random()*5);
-	var x = Math.floor(Math.random()*5);
-	var y = Math.floor(Math.random()*5);
-    var z = Math.floor(Math.random()*5);
-	
-	// Modification de la source des images à l'aide des 3 nombres aléatoires
-	document.getElementById("images/carreau.png").src = 'img_' + w + '.png';
-	document.getElementById("images/coeur.png").src = 'img_' + x + '.png';
-	document.getElementById("images/pique.png").src = 'img_' + y + '.png';
-	document.getElementById("images/trefle.png").src = 'img_' + z + '.png';
-	
-	// Le joueur perd 1 crédit à chaque tirage
-	creditJoueur -= 1;
-	
-	
-	// Si les 3 nombres sont égaux, alors on a 3 mêmes images et l'utilisateur gagne 80
-	if(w == x && x == y && y==z){
-		creditJoueur += 5;
-	}
-	/* Si l'utilisateur a un crédit plus élevé que son highscore, alors le highscore
-	prend la valeur du crédit */
-	if(credit > score){
-		score = credit;
-	}
-	/* On met à jour le crédit et le highscore du joueur */
-	document.getElementById("credit").value = credit;
-	document.getElementById("score").value = score;
-}
+var w = {};
+var x = {};
+var y = {};
+var z = {};
+
+$("button[id='boutonGO']").click(function() {
+    console.log("Everything is all right")
+        //Lancer la fonction 1
+});
+
+//Fonction 1
+
+//Fonction 2
+
+//Petit codage surprise.
